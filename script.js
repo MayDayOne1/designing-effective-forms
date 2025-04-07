6c8204abd1a59d9aa9d7f86152656f69772e7007
pmilowski <pmilowski16@gmail.com>
2025-04-07 12:28:08 +0200

    get country and code by id

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const myForm = document.getElementById('form');
 const modal = document.getElementById('form-feedback-modal');
 const clicksInfo = document.getElementById('click-count');
 
+window.onload = getCountryByIP()
+
 function handleClick() {
     clickCount++;
     clicksInfo.innerText = clickCount;
@@ -29,7 +31,9 @@ function getCountryByIP() {
         .then(response => response.json())
         .then(data => {
             const country = data.country;
-            // TODO inject country to form and call getCountryCode(country) function
+            countrySelector = document.getElementById("country");
+            countrySelector.value = country;
+            getCountryCode(country);
         })
         .catch(error => {
             console.error('Błąd pobierania danych z serwera GeoJS:', error);
@@ -48,7 +52,8 @@ function getCountryCode(countryName) {
     })
     .then(data => {        
         const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
-        // TODO inject countryCode to form
+        countryCodeSelector = document.getElementById("countryCode");
+        countryCodeSelector.value = countryCode;
     })
     .catch(error => {
         console.error('Wystąpił błąd:', error);
